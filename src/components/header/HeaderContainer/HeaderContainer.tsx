4e9dac8d35269dff88adf9e753f5bf8a233ed69b
TetianaDiachenko <tetianamyhun@gmail.com>
2025-06-28 19:40:15 +0300

    added Search component

--- src/components/header/HeaderContainer/HeaderContainer.tsx
+++ src/components/header/HeaderContainer/HeaderContainer.tsx
@@ -1,12 +1,14 @@
 import styles from './HeaderContainer.module.scss';
 import Logo from '../Logo/Logo';
+import Search from '../Search/Search';
 
 const HeaderContainer = () => {
 	return (
 		<header className={styles.header}>
 			<div className={styles.wrapper}>
-				<div className="left">
+				<div className={styles.left}>
 					<Logo />
+					<Search />
 				</div>
 				<div className="right">
 					
