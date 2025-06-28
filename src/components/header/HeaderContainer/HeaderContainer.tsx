264381cdbd7c2b8e43754b5a890766055cfe187d
TetianaDiachenko <tetianamyhun@gmail.com>
2025-06-28 18:36:53 +0300

    added Logo component to the left div in Header

--- src/components/header/HeaderContainer/HeaderContainer.tsx
+++ src/components/header/HeaderContainer/HeaderContainer.tsx
@@ -1,10 +1,16 @@
 import styles from './HeaderContainer.module.scss';
-
+import Logo from '../Logo/Logo';
 
 const HeaderContainer = () => {
 	return (
 		<header className={styles.header}>
 			<div className={styles.wrapper}>
+				<div className="left">
+					<Logo />
+				</div>
+				<div className="right">
+					
+				</div>
 			</div>
 		</header>
 	);
