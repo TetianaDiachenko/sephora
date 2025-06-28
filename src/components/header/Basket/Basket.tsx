1c6e111b0c32d5608686607efa94586d383c7277
TetianaDiachenko <tetianamyhun@gmail.com>
2025-06-28 22:54:28 +0300

    added Basket component

---
+++ src/components/header/Basket/Basket.tsx
@@ -0,0 +1,14 @@
+import styles from './Basket.module.scss';
+import Bin from '../../svg-components/Bin';
+
+const Basket = () => {
+	return (
+		<div className={styles.basket}>
+			<a href="/">
+				<Bin />
+			</a>
+		</div>
+	);
+};
+
+export default Basket; 
