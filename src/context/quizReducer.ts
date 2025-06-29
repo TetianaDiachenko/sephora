c508dacb51402da5c49bd6aa61012c82a634a7c8
TetianaDiachenko <tetianamyhun@gmail.com>
2025-06-29 20:48:16 +0300

    added Reduser and type in quizReduser

---
+++ src/context/quizReducer.ts
@@ -0,0 +1,39 @@
+export type Answer = string;
+
+export interface QuizState {
+  step: number;
+  answers: Answer[];
+  isFinished: boolean;
+}
+
+export type QuizAction =
+  | { type: 'ANSWER'; payload: Answer }
+  | { type: 'NEXT_STEP' }
+  | { type: 'RESET' };
+
+export const initialQuizState: QuizState = {
+  step: 0,
+  answers: [],
+  isFinished: false,
+};
+
+export function quizReducer(state: QuizState, action: QuizAction): QuizState {
+  switch (action.type) {
+    case 'ANSWER':
+      return {
+        ...state,
+        answers: [...state.answers, action.payload],
+      };
+    case 'NEXT_STEP':
+      const isLast = state.step >= 2; 
+      return {
+        ...state,
+        step: state.step + 1,
+        isFinished: isLast,
+      };
+    case 'RESET':
+      return initialQuizState;
+    default:
+      return state;
+  }
+}
